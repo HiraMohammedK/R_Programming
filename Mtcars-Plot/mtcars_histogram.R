cd740c89553fd44cfe110a02f5395f0935203c7f
HiraMohammedK <98226271+HiraMohammedK@users.noreply.github.com>
2023-11-03 13:06:58 +0530

    new

--- Mtcars-Plot/mtcars_histogram.R
+++ Mtcars-Plot/mtcars_histogram.R
@@ -2,7 +2,7 @@ mtcars_data <- read.csv("mtcars.csv")
 mpg_mean <- mean(mtcars_data$mpg)
 mpg_sd <- sd(mtcars_data$mpg)
 
-hist(mtcars_data$mpg, col = "darkgreen", main = "Histogram of Miles Per Gallon (mpg)",
+hist(mtcars_data$mpg, col = c("blue","lightblue","darkblue","cyan","cornflowerblue"), main = "Histogram of Miles Per Gallon (mpg)",
      xlab = "Miles per Gallon (mpg)", ylab = "Frequency")
 
 legend("topright", legend = c(paste("Mean:", round(mpg_mean, 2)), paste("SD:", round(mpg_sd, 2))),
